3d89741cf6e8448dc7ead0dad475424aea920c5b
Martinique Dolce <me@martidolce.com>
2020-12-11 11:22:00 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,13 @@
-require 'bundler'
+# frozen_string_literal: true
+
+
+require 'sqlite3'
+require 'pry'
 Bundler.require
 
+require_relative '../lib/student'
+
+require 'sqlite3'
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
